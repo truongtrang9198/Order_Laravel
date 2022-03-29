c0426ae1bd391e4961f1d90046aad85cbb7d8340
truongtrang <truongtrang9198@gmail.com>
2022-03-29 22:40:06 +0700

    Create Header.blade.php

---
+++ resources/views/Homepage/Header.blade.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xm-12 col-md-12" id="top-header">
+            <div>
+            <img src="{{asset('order.png')}}" alt="" >
+            <br>
+             <span class="text-light"><i class="fas fa-user-check"></i> Khách</span>
+            </div>
+            <div class="slide_img">
+
+            </div>
+        </div>
+    </div>
+
+</div>
