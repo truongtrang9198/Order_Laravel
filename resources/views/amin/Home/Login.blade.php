1b50974f44131ee2d44817a9839564d194f507a9
truongtrang <truongtrang9198@gmail.com>
2022-03-29 22:40:08 +0700

    Update Login.blade.php

--- resources/views/amin/Home/Login.blade.php
+++ resources/views/amin/Home/Login.blade.php
@@ -2,26 +2,32 @@
 
 <div id="login-frame">
     <div id="img_login">
-        <img  src="{{asset('/storage/app/img/order.png')}}" alt="Hình ảnh logo">
+        <img src="{{ asset('http://localhost/Order_Laravel/storage/app/img/order.png') }}" alt="Hình ảnh logo">
     </div>
     <div id="form-login-admin">
-        <form action="{{route('getlogin')}}" method="GET">
-        @csrf
+        <form action="{{ route('getlogin') }}" method="GET">
+            @csrf
             <div class="input-group mb3 input-admin">
                 <div class="input-group-prepend">
-                  <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                    <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
                 </div>
-                <input type="text" class="form-control input-admin"  name="phone" aria-describedby="basic-addon1">
-              </div>
-              <br>
-              <div class="input-group mb3 input-admin">
+                <input type="text" class="form-control input-admin" placeholder="Phone number" name="phone"
+                    aria-describedby="basic-addon1">
+            </div>
+            <br>
+            <div class="input-group mb3 input-admin">
                 <div class="input-group-prepend">
-                  <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
+                    <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
                 </div>
-                <input type="text" class="form-control input-admin" placeholder="Password" name="password"  aria-describedby="basic-addon1">
-              </div>
-              <br>
-              <button type="submit" class="btn btn-admin-login">Login</button>
+                <input type="password" class="form-control input-admin" placeholder="Password" name="password"
+                    aria-describedby="basic-addon1">
+            </div>
+            @if ($errors->any())
+                <span class="text-muted">{{ $errors->first() }}</span>
+                @endif
+
+                <br>
+                <button type="submit" class="btn btn-admin-login">Login</button>
         </form>
     </div>
 
