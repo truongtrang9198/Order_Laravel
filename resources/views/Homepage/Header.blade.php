b0960348b52e443d586e02ffd36e53a1682bce42
truongtrang <truongtrang9198@gmail.com>
2022-04-04 08:10:08 +0700

    Update Header.blade.php

--- resources/views/Homepage/Header.blade.php
+++ resources/views/Homepage/Header.blade.php
@@ -1,10 +1,20 @@
+
+@php
+    if (session('username') !=''){
+        $user = session('username');
+    }else{
+        $user = "Khách";
+    }
+@endphp
 <div class="container-fluid">
     <div class="row">
         <div class="col-xm-12 col-md-12" id="top-header">
             <div>
             <img src="{{asset('order.png')}}" alt="" >
             <br>
-             <span class="text-light"><i class="fas fa-user-check"></i> Khách</span>
+             <span class="text-light"><i class="fas fa-user-check"></i> @php
+                 echo $user;
+             @endphp</span>
             </div>
             <div class="slide_img">
 
