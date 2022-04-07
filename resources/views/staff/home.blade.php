a1322e1f3982d7ef70a45886499d041156587a91
truongtrang <truongtrang9198@gmail.com>
2022-04-07 22:56:55 +0700

    Create home.blade.php

---
+++ resources/views/staff/home.blade.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('Homepage.head')
+</head>
+<body class="body">
+    <div class="fluid-container">
+        <div class="header">
+            @include('Homepage.Header')
+        </div>
+        <hr>
+    </div>
+        @yield('main')
+
+</body>
+</html>
+
+<div class="toast" id="Thongbaoloi" data-autohide="false">
+    <div class="toast-header">
+       <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">&times;</button>
+       <b class="text-danger">Hiển thị thông báo </b>
+    </div>
+    <div class="toast-body text-danger" id="toast-content-err">
+
+    </div>
+</div>
