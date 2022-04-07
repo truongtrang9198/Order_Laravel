9ae164bd67cd93a325c4a87179c1cae056a090dd
truongtrang <truongtrang9198@gmail.com>
2022-04-07 22:53:17 +0700

    Update BillModel.php

--- app/Models/BillModel.php
+++ app/Models/BillModel.php
@@ -11,4 +11,18 @@ class BillModel extends Model
     protected $table  = 'bill';
     protected $primaryKey = 'ID_BILL';
     public $timestamps = false;
+
+//relationship
+    public function order_detail()
+    {
+        return $this->hasMany('App\Models\DetailModel','ID_BILL','ID_BILL');
+    }
+
+    public function getid($id){
+        $data = BillModel::where('ID_TABLE',$id)
+                        ->where('BILL_STATUS','Chưa thanh toán')
+                        ->get('ID_BILL');
+
+        return $data;
+    }
 }
