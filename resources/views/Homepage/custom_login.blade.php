4f4c007b975487155e4e3b95086ef9ea79f8185c
truongtrang <truongtrang9198@gmail.com>
2022-04-07 22:57:09 +0700

    Update custom_login.blade.php

--- resources/views/Homepage/custom_login.blade.php
+++ resources/views/Homepage/custom_login.blade.php
@@ -5,13 +5,16 @@
 <div class="container">
     <div class="row">
         <div class="col-xm-12">
+            <h4 class="text-center">Đăng nhập để tích điểm</h4>
           <form action="{{route('custom_login')}}" method="POST" class="form-group" id="form-login">
-                <label for="">Tên</label>
+            @csrf
+            <label for="">Tên</label>
                 <input type="text" id="name_cutomer" name="name_cutomer" class="form-control">
                 <label for="">Số điện thoại</label>
                 <input type="text" id="phone_cutomer" name="phone_cutomer"class="form-control">
             <br>
                 <button class="btn btn-warning" type="submit">Tiếp</button>
+                <button class="btn btn-info" onClick="{{route('choose_table')}}">Bỏ qua</button>
           </form>
         </div>
 
@@ -24,5 +27,10 @@
 <style>
     #form-login{
         padding-left: 100px;
+        font-family: Arial, Helvetica, sans-serif;
     }
+    h4{
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
 </style>
