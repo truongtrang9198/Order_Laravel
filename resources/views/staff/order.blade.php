ab4b5cdfb6ad885d0ce4396aa328e8b1edf029e7
truongtrang <truongtrang9198@gmail.com>
2022-04-07 22:56:49 +0700

    Create order.blade.php

---
+++ resources/views/staff/order.blade.php
@@ -0,0 +1,72 @@
+@extends('staff.home')
+@section('tile', 'Bếp')
+@section('main')
+    @parent
+
+
+            <table class="table table-striped table-hover">
+                <thead class="thead-inverse">
+                    <tr>
+                        <th>Tên món</th>
+                        <th>Số bàn</th>
+                        <th>Ghi chú</th>
+                        <th>Thời gian</th>
+                        <th>Trạng thái</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            foreach($data as $dt){
+                                $str = '
+                                    <tr>
+                                    <td>'.$dt->DISH_NAME.'</td>
+                                    <td>'.$dt->NUMBER_TABLE.'</td>
+                                    <td>'.$dt->NOTE.'</td>
+                                    <td>'.$dt->TIME_ORDER.'</td>
+                                    <td>'.$dt->STATUS_DETAIL.'</td>
+                                    <form action="" class="form-confirm">
+                                    <td>
+                                        <select name="status" id="" class="custom-select">
+                                            <option value="Hoàn tất">Hoàn tất</option>
+                                            <option value="Hết món">Hết món</option>
+
+                                        </select>
+                                    </td>
+
+                                    <td><button type="submit" value="'.$dt->ID_DETAIL.'"  class="btn btn-outline-primary btn-click">Xác nhận</button></td>
+                                </form>
+                                    </tr>
+                                ';
+                                echo $str;
+                            }
+                        @endphp
+
+                    </tbody>
+            </table>
+
+ @endsection
+
+ <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+ integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+ <meta name="csrf-token" content="{{ csrf_token() }}" />
+ <script>
+     $(document).ready(function() {
+         // check img
+
+         // $('.btn').click(function(event){
+         //     event.preventDefault();
+         // })
+
+         $.ajaxSetup({
+             headers: {
+                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+             }
+         });
+        $(".form-confirm").submit(function(event){
+            event.preventDefault();
+
+        });
+     })
+ </script>
