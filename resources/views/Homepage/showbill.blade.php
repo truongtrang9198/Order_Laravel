c9938378239b9b4d6989b1c10fd6ecd5a583391d
truongtrang <truongtrang9198@gmail.com>
2022-04-14 23:22:30 +0700

    Update showbill.blade.php

--- resources/views/Homepage/showbill.blade.php
+++ resources/views/Homepage/showbill.blade.php
@@ -18,7 +18,7 @@
                         @php
 
                             $n = 0;
-                            foreach ($items as $dt) {
+                            foreach ($detail as $dt) {
                                 $n++;
                                 $tr =
                                     "<tr>
@@ -42,22 +42,31 @@
                             }
 
                         @endphp
-                        <tr>
-                            <td scope="row">Tổng</td>
-                            <td colspan="2" class="text-right"> <b>@php
-                                echo number_format($dt->TOTAL);
-                            @endphp </b></td>
-                        </tr>
-                        <tr>
-                            <td scope="row">Trạng thái</td>
-                            <td colspan="2" class="text-right text-muted"> <i>@php
-                                echo $dt->BILL_STATUS;
-                            @endphp </i> </td>
-                        </tr>
+                        @php
+                            foreach($bill as $b){
+                               $tr ='  <tr>
+                                <td scope="row">Tổng</td>
+                                <td colspan="2" class="text-right"> <b> '.number_format($b->TOTAL).'
+                                     </b></td>
+                                     </tr>
+                                <tr>
+
+                                <td scope="row">Trạng thái</td>
+                                <td colspan="2" class="text-right text-muted"> <i>'.$b->BILL_STATUS.'
+                                </i> </td>
+
+                            </tr>';
+                            echo $tr;
+                            $s = '<input type="text" id="id_bill" value="'. $b->ID_BILL.'" hidden>';
+                            echo $s;
+                            }
+                        @endphp
+
+
                     </tbody>
                 </table>
                 <button type="button" onclick="history.back()" class="btn btn-danger">Trở về</button>
-                <button type="submit" class="btn btn-warning">Thanh toán</button>
+                <button type="submit" id="pay-btn" class="btn btn-warning">Thanh toán</button>
             </div>
         </div>
     </div>
@@ -68,3 +77,20 @@
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
 integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function(){
+        $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+        $('#pay-btn').click(function(){
+            let id_bill = $('#id_bill').val();
+            $.post("{{route('get-confirm')}}",{id_bill:id_bill},function(data){
+                location.reload();
+                console.log(data);
+            })
+           // alert(id_bill);
+        })
+    });
+</script>
