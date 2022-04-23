dffeb5ffca64af174a45d0cb1f6a80218b25c441
truongtrang <truongtrang9198@gmail.com>
2022-04-23 08:13:44 +0700

    Create bill.js

---
+++ resources/js/customscript/bill.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+    $('#pay-btn').click(function(){
+        let id_bill = $('#id_bill').val();
+        $.post("{{route('get-confirm')}}",{id_bill:id_bill},function(data){
+            location.reload();
+            console.log(data);
+        })
+       // alert(id_bill);
+    })
+
+    // hiển thị button đánh giá sau khi đã thanh toán
+    var status = $('#status_bill').val();
+
+    if(status =="Đã thanh toán"){
+        $.get("{{route('check_condition')}}",{},function(data){
+            if(data=="null"){
+                // open modal dang nhap
+
+                $('#modal_login').modal('show');
+            }else{
+                $('#hidden_cmt').collapse('show');
+
+            }
+        })
+
+    }
+
+});
