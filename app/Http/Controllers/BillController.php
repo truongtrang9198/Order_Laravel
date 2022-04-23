adaadcce041e101c086959677c5479fc707ea020
truongtrang <truongtrang9198@gmail.com>
2022-04-23 08:13:21 +0700

    Update BillController.php

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BillModel as BillModel;
+use App\Models\PaymentModel as PaymentModel;
+use Carbon\Carbon;
+use App\Models\Table_order as Table_order;
 class BillController extends Controller
 {
     public function get_confirm(Request $request){
@@ -20,6 +23,27 @@ class BillController extends Controller
     public function page_payment(){
         $data = new BillModel();
         $dt = $data->get_all_bill();
-        return view('staff.page_payment',["items"=>$dt]);
+        $paymentItems = PaymentModel::all();
+       // return $dt;
+        return view('staff.page_payment',["items"=>$dt,"payment"=>$paymentItems]);
+    }
+
+    public function confirmed(Request $re){
+       // đặt trạng thái trong bảng bill
+        $id_bill = $re->id_bill;
+        $date = Carbon::now('Asia/Ho_Chi_Minh');
+        $data = BillModel::find($id_bill);
+        $data->PAY = $re->paid;
+        $data->ID_PAYMENT   = $re->payment_type ;
+        $data->BILL_STATUS  = "Đã thanh toán";
+        $data->	Time_end  = $date;
+        $data->save();
+       // đặt trạng thái trong bảng table
+
+       $id_table = $re->id_table;
+       $table = Table_order::find($id_table);
+       $table->STATUS = "Sẵn sàng";
+       $table->save();
+        return "Success";
     }
 }
