ec996fa0819793bc78a29959e2818b56749b8055
truongtrang <truongtrang9198@gmail.com>
2022-04-23 08:13:47 +0700

    Update custom_login.blade.php

--- resources/views/Homepage/custom_login.blade.php
+++ resources/views/Homepage/custom_login.blade.php
@@ -9,9 +9,9 @@
           <form action="{{route('custom_login')}}" method="POST" class="form-group" id="form-login">
             @csrf
             <label for="">Tên</label>
-                <input type="text" id="name_cutomer" name="name_cutomer" class="form-control">
+                <input type="text" id="name" name="name" class="form-control">
                 <label for="">Số điện thoại</label>
-                <input type="text" id="phone_cutomer" name="phone_cutomer"class="form-control">
+                <input type="text" id="phone" name="phone"class="form-control">
             <br>
                 <button class="btn btn-warning" type="submit">Tiếp</button>
                 <button class="btn btn-info" onClick="{{route('choose_table')}}">Bỏ qua</button>
