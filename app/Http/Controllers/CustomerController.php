d9b7a239a82a88b029d07bc820354e725d168365
truongtrang <truongtrang9198@gmail.com>
2022-04-23 08:13:27 +0700

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,26 +4,61 @@ namespace App\Http\Controllers;
 use App\Models\CustomerModel as CustomerModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-
+use DB;
 class CustomerController extends Controller
 {
     public function custom_login(Request $request){
         $customer = new CustomerModel();
         $data = $request->input();
-        $phone = $data['phone_cutomer'];
-        $name = $data['name_cutomer'];
-        $condi = CustomerModel::where('PHONE',$phone)->count();
-        if( $condi == 0){
+        $phone = $request->phone;
+        $name = $request->name;
+        $condition = CustomerModel::where('PHONE',$phone)->count();
+        if( $condition == 0){
             $customer->PHONE = $phone;
             $customer->FULLNAME = $name;
             $customer->POINT_TOTAL	= 0;
 
             $customer->save();
+            $id_customer = DB::getPdo()->lastInsertId();
+        }else{
+            $items = CustomerModel::where('PHONE',$phone)
+                    ->get();
+            foreach ($items as $item)
+                $id_customer = $item->PHONE;
         }
     // lưu trữ session
        // session(['username' => ]);
-        Session::push('username',$name);
+        session(['username' => $name]);
+        session(['id_customer' => $id_customer]);
         return redirect()->route('choose_table');
+
+    }
+
+    public function custom_login2(Request $request){
+
+        $customer = new CustomerModel();
+        $data = $request->input();
+        $phone = $data['phone'];
+        $name = $data['name'];
+        $condition = CustomerModel::where('PHONE',$phone)->count();
+        if( $condition == 0){
+            $customer->PHONE = $phone;
+            $customer->FULLNAME = $name;
+            $customer->POINT_TOTAL	= 0;
+
+            $customer->save();
+            $id_customer = DB::getPdo()->lastInsertId();
+        }else{
+            $items = CustomerModel::where('PHONE',$phone)
+                    ->get();
+            foreach ($items as $item)
+                $id_customer = $item->PHONE;
+        }
+    // lưu trữ session
+       // session(['username' => ]);
+        session(['username' => $name]);
+        session(['id_customer' => $id_customer]);
+        return session('username');
 
     }
 }
