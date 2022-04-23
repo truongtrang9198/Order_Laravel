6f387c8541cd3d5b47ee4e3e7a153648a4f77f0f
truongtrang <truongtrang9198@gmail.com>
2022-04-23 08:14:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\admin\ManageTable;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\BillController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,7 @@ Route::get('/', function () {
     return view("Homepage.custom_login");
 });
 Route::post('custom_login/',[CustomerController::class,'custom_login'] )->name('custom_login');
+Route::post('custom_login2/',[CustomerController::class,'custom_login2'] )->name('login2');
 
 Route::get('detail_menu/{id}',[ManageMenu::class,'detail_menu'])->name('detail_menu')
 ->where('id','[0-9]+');
@@ -32,7 +34,12 @@ Route::get('menu/{id_table}/{number_table}/',[ManageMenu::class,'_menu'])->name(
 Route::post('add_order/',[DetailController::class,'add_order'])->name('add_order');
 Route::get('show_bill/{id_bill}',[DetailController::class,'show_bill'])->name('show_bill');
 Route::get('order_process/',[DetailController::class,'order_process'])->name('order_process');
-Route::post('confirm/',[DetailController::class,'confirm'])->name('confirm');
+Route::post('Staff/confirm/',[DetailController::class,'confirm'])->name('confirm');
 
-Route::post('get-confirm/',[BillController::class,'get_confirm'])->name('get-confirm');
-Route::get('page_payment/',[BillController::class,'page_payment'])->name('page_payment');
+Route::post('Staff/get-confirm/',[BillController::class,'get_confirm'])->name('get-confirm');
+Route::get('Staff/page_payment/',[BillController::class,'page_payment'])->name('page_payment');
+Route::post('Staff/confirmed/',[BillController::class,'confirmed'])->name('confirmed');
+Route::get('Comment/check_condition',[CommentController::class,'check_condition'])->name('check_condition');
+// Route::get('Comment/{id_bill}',[CommentController::class,'check_condition'])->name('go_cmt')
+// ->where('id_bill','[0-9]+');
+Route::get('Comment/comment/{id_bill}',[CommentController::class,'go_comment'])->name('go_cmt');
