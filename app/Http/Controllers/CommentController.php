cade61d60d93223591d5eee1019130b5bafff84d
truongtrang <truongtrang9198@gmail.com>
2022-04-27 19:23:29 +0700

    Update CommentController.php

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CommentModel as CommentModel;
+use App\Models\DetailModel as DetailModel;
 use Session;
 use DB;
+use Carbon\Carbon;
 class CommentController extends Controller
 {
     //
@@ -19,7 +22,31 @@ class CommentController extends Controller
 
     public function go_comment($id_bill){
         $data = DB::select("call show_menu_comment($id_bill)");
+        $count = DetailModel::where('ID_BILL',$id_bill)
+                                ->where('comment_check',0)
+                                ->count();
         //dd($data);
-        return view('Homepage.rating',["data"=>$data]);
+        return view('Homepage.rating',["data"=>$data,"count"=>$count]);
     }
+
+    public function insert_cmt(Request $re){
+        $id_dish = $re->id_dish;
+        $content = $re->content;
+        $rating =  $re->rating;
+        $id_bill = $re->id_bill;
+        $id_detail = $re->id_detail;
+        $date = Carbon::now('Asia/Ho_Chi_Minh');
+        $id_custom = session('id_customer');
+
+        DB::insert('insert into comment (ID_CUSTOMERS,ID_DISH,COMMENT_DETAIL,
+        RATE,TIME) values (?, ?,?,?,?)', [$id_custom,$id_dish,$content,$rating,$date]);
+
+       /// DB::select("call change_comment_check($id_bill,$id_dish)");
+       $data = DetailModel::find($id_detail);
+       $data->comment_check = 1;
+       $data->save();
+
+        return "Success";
+    }
+
 }
