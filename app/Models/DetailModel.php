1d91f790d2b50704a623a50084e85172e3302a79
truongtrang <truongtrang9198@gmail.com>
2022-04-28 23:47:55 +0700

    Update DetailModel.php

--- app/Models/DetailModel.php
+++ app/Models/DetailModel.php
@@ -23,23 +23,7 @@ class DetailModel extends Model
         return $this->belongsTo('App\Models\BillModel','ID_BILL','ID_BILL');
     }
 
-    public function getBill($id_bill){
 
-        $detail = DB::select("call show_bill(".$id_bill.")");
-        $bill = DB::select("select ID_BILL,TOTAL,BILL_STATUS from bill where ID_BILL = $id_bill");
-
-    //     $data = DetailModel::with('menu')->where('ID_BILL',$id_bill)
-    //                                     ->get();
-    //    //$total = DetailModel::with('bill')->where('ID_BILL',$id_bill)
-    //                                //     ->get();
-    //     $totals = BillModel::find($id_bill);
-    //     $total = $totals->TOTAL;
-    //     $bill_status = $totals->BILL_STATUS;
-    //    // $arr['menu'] = $data;
-    //   //  $arr[1] = $total;
-    //     return ["data"=>$data,'total'=>$total,"bill_status"=>$bill_status];
-        return ["detail"=>$detail,"bill"=>$bill];
-    }
 
     public function order(){
         $data = DB::select("call show_all_order()");
