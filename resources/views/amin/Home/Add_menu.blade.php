7fe060abba9910b4c17c09ec1e0afa503bfe3a82
truongtrang <truongtrang9198@gmail.com>
2022-05-04 00:25:55 +0700

    Update Add_menu.blade.php

--- resources/views/amin/Home/Add_menu.blade.php
+++ resources/views/amin/Home/Add_menu.blade.php
@@ -1,4 +1,4 @@
-<?php echo csrf_field(); ?>
+
 @extends('amin.Home')
 @section('tile', 'Quản lý menu')
 @section('main')
@@ -74,7 +74,7 @@
 
 @endsection
 
-<meta name="csrf-token" content="{{ csrf_token() }}" />
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
 integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
