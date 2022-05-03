da2c807e893b1ea7a1c1d7896edf8fb01e10b2ff
truongtrang <truongtrang9198@gmail.com>
2022-05-03 08:54:12 +0700

    Update detail_menu.blade.php

--- resources/views/Homepage/detail_menu.blade.php
+++ resources/views/Homepage/detail_menu.blade.php
@@ -1,5 +1,5 @@
-
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{asset('css/Detail_comment.css')}}">
 @extends('Homepage.Home')
 @section('tile', 'Welcome')
 @section('main')
@@ -7,6 +7,8 @@
 <div class="container">
     <div class="row">
         <div class="col-xm-12">
+            <button class="btn btn-primary" id="btn_back" onclick="history.back()"><i class="fas fa-backward"></i></button>
+            <br>
             @php
             foreach($data as $data){
                 $price = number_format($data->DISH_PRICE);
@@ -29,7 +31,33 @@
         </div>
         <div>
             <p>Khu vực hiển thị bình luận</p>
+            <span id="">
+                &nbsp;
+                 <button id="fa-frown" class="btn btn-rating" data-toggle="tooltip"data-placement="bottom" title="Rất tệ" disabled="disabled">
+                     <i class="far fa-frown" style="font-size:34px"></i> {{$rating_bad}}</button> &nbsp;
+                 <button id="fa-smile" class="btn btn-rating" data-toggle="tooltip"data-placement="bottom" title="Bình thường thôi" disabled="disabled">
+                    <i class="far fa-smile"style="font-size:34px" ></i> {{$rating_normal}}</button> &nbsp;
+                 <button id="fa-heart" class="btn btn-rating" data-toggle="tooltip"data-placement="bottom" title="Ngon tuyệt" disabled="disabled">
+                     <i class="far fa-grin-hearts"style="font-size:34px"></i> {{$rating_wonder}}</button>
+            </span>
+            <br>
+            {{-- Hiển thị bình luận --}}
+            <div class="comment">
+              @foreach ($comment as $cmt)
+
+                    <div class="card">
+                        <div class="body">
+                            <p class="fort-weight-bold"><i class="fas fa-user-tag"></i> {{$cmt->FULLNAME}}
+                                <br><small class="text-muted"><i class="fas fa-clock"></i>{{$cmt->TIME}} </small></p>
+                             <p>{{$cmt->COMMENT_DETAIL}}</p>
+                        </div>
+                    </div>
+                    <br>
+
+                 @endforeach
+             </div>
         </div>
+
     </div>
 </div>
 
