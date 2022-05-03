e1cd766c87f1fa0f44d4fe19c4c479a1cadbac97
truongtrang <truongtrang9198@gmail.com>
2022-05-03 08:54:08 +0700

    Update bill.js

--- public/js/bill.js
+++ public/js/bill.js
@@ -1,33 +1,17 @@
 $(document).ready(function(){
     $.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-});
-    $('#pay-btn').click(function(){
-        let id_bill = $('#id_bill').val();
-        $.post("{{route('get-confirm')}}",{id_bill:id_bill},function(data){
-            location.reload();
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+        $('#btn_update').click(function(){
+            var ne_name = $('#ne_username').val();
+        $.post("user_update/",{ne_name:ne_name},function(data){
             console.log(data);
-        })
-       // alert(id_bill);
-    })
-
-    // hiển thị button đánh giá sau khi đã thanh toán
-    var status = $('#status_bill').val();
-
-    if(status =="Đã thanh toán"){
-        $.get("{{route('check_condition')}}",{status:status},function(data){
-            if(data=="null"){
-                // open modal dang nhap
-
-                $('#modal_login').modal('show');
-            }else{
-                $('#hidden_cmt').collapse('show');
-
-            }
+            location.reload();
         })
 
-    }
+        })
+})
 
-});
+
