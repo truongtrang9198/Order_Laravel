61142b183d38e49938de8267dda0144b9ef4b1da
truongtrang <truongtrang9198@gmail.com>
2022-05-03 08:54:53 +0700

    Update CommentModel.php

--- app/Models/CommentModel.php
+++ app/Models/CommentModel.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use DB;
 class CommentModel extends Model
 {
 
     protected $table  = 'comment';
     protected $primaryKey = 'ID_COMMENT';
     public $timestamps = false;
+// Lấy bình luận và tên khách hàng
+    public function get_cmt($id){
+        $result = DB::select("call get_comment($id)");
+        return $result;
+    }
 
 }
