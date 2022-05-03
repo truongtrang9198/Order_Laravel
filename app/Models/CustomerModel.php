a7c2e49d60381cc4f302b5640d94c2a14c7cdc01
truongtrang <truongtrang9198@gmail.com>
2022-05-03 08:54:51 +0700

    Update CustomerModel.php

--- app/Models/CustomerModel.php
+++ app/Models/CustomerModel.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use DB;
 class CustomerModel extends Model
 {
     use HasFactory;
     protected $table  = 'customers';
     protected $primaryKey = 'ID_CUSTOMERS';
     public $timestamps = false;
+
+    public function Total($id_customer){
+        $result = DB::select("call money_spent($id_customer)");
+        return $result;
+    }
+
+    public function customer_history($id_customer){
+        $result = DB::select("call customer_history($id_customer)");
+        return $result;
+    }
 }
